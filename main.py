57b38cd1b682095b4a79dbcb0f3cf69bb207d18f
Alirex Prime <alirex.prime@gmail.com>
2022-05-29 14:02:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    print('Hi')
+def example_1(arg1: str) -> str:
+    return arg1 if arg1 != '' else 'default'
+
+
+def example_2(arg1: str) -> str:
+    return arg1 or 'default'
+
+
+def main() -> None:
+    print(example_1(''))
+    print(example_1('value'))
+
+    print(example_2(''))
+    print(example_2('value'))
 
 
 if __name__ == '__main__':
